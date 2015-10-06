37173cc2d12bed3de3abe45d2820430cabc20de2
medhi <medhiwidjaja@yahoo.com>
2015-10-06 14:16:19 +0700

    Experiment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,20 @@ class SessionsController < ApplicationController
     begin
       puts "USER begin"
       # @user = User.from_omniauth(request.env['omniauth.auth'])
-      @user = User.new
-      @user.id = request.env['omniauth.auth']['uid']
-      @user.name = request.env['omniauth.auth']['info']['name']
-      puts "USER done"
-      puts @user.to_yaml
+      # @user = User.new
+      # @user.id = request.env['omniauth.auth']['uid']
+      # @user.name = request.env['omniauth.auth']['info']['name']
+      # puts "USER done"
+      # puts @user.to_yaml
+
+      @user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+      @user.name = auth_hash['info']['name']
+      @user.email = auth_hash['info']['email']
+      @user.image = auth_hash['info']['image']
+      @user.first_name = auth_hash['info']['first_name']
+      @user.last_name = auth_hash['info']['last_name']
+      @user.url = auth_hash['info']['urls'][@user.provider.capitalize]
+      @user.save!
       session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.name}!"
     rescue
