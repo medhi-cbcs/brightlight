84b7561113d71eaad3bfc1a9af24f54bf84fad30
medhi <medhiwidjaja@yahoo.com>
2015-10-07 15:36:23 +0700

    academic test end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,7 @@
 class SessionsController < ApplicationController
   def create
     begin
-      puts "USER begin"
-      # @user = User.from_omniauth(request.env['omniauth.auth'])
-      # @user = User.new
-      # @user.id = request.env['omniauth.auth']['uid']
-      # @user.name = request.env['omniauth.auth']['info']['name']
-      # puts "USER done"
-      # puts @user.to_yaml
+
       auth_hash = request.env['omniauth.auth']
       @user = User.find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
       @user.name = auth_hash['info']['name']
@@ -21,7 +15,6 @@ class SessionsController < ApplicationController
       flash[:success] = "Welcome, #{@user.name}!"
     rescue
       flash[:warning] = "There was an error while trying to authenticate you..."
-      puts "OMNIAUTH"
       puts request.env["omniauth.auth"].to_yaml
     end
     redirect_to books_path
