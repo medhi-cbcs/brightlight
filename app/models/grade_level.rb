60e8a348f28f79d1ed53e6d5d4fae80e8d69d318
medhi <medhiwidjaja@yahoo.com>
2015-11-12 15:51:35 +0700

    Interim commit. Grade level using cocoon nested form

--- app/models/grade_level.rb
+++ app/models/grade_level.rb
@@ -1,4 +1,4 @@
 class GradeLevel < ActiveRecord::Base
 	has_many :grade_sections, dependent: :destroy
-  accepts_nested_attributes_for :grade_sections, allow_destroy: true
+  accepts_nested_attributes_for :grade_sections, allow_destroy: true, reject_if: :all_blank
 end
