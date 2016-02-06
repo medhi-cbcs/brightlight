d7f61b618da6f5b4d248c81952768fa854080ee2
Medhi Widjaja <medhiwidjaja@yahoo.com>
2016-02-06 23:49:41 +0700

    CopyCondition Javascript menu for choosing label

--- app/models/book_label.rb
+++ app/models/book_label.rb
@@ -1,12 +1,24 @@
 # Book Labels are in the form of GradeSection.name#number, such as '1A#12' or '12C#3'
 class BookLabel < ActiveRecord::Base
-  belongs_to :grade_section
+  belongs_to :grade_level
   belongs_to :student
 
   validates :name, presence: true, uniqueness: true
 
+  def self.for_name(name)
+    where(name:name).first
+  end
+
   def self.for_section_and_number(section, number)
     label = section + '#' + number.to_s
     where(name:label).first
+  end
+
+  def self.for_section(section)
+    where('name LIKE ?', "#{section}%")
+  end
+
+  def section_name
+    name.match(/\d+[A-Z]/).to_s
   end
 end
