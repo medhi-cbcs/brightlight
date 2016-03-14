3ce27b9e0451e10c6a1f467de81e87d3655297fc
Medhi Widjaja <medhiwidjaja@yahoo.com>
2016-03-15 01:10:21 +0700

    Interim commit. Importing grade section attributes

--- app/models/grade_section.rb
+++ app/models/grade_section.rb
@@ -12,6 +12,7 @@ class GradeSection < ActiveRecord::Base
   has_many :students, through: :grade_sections_students
   has_many :student_books
   has_many :book_labels
+  has_many :grade_section_histories
 
   scope :with_academic_year, lambda {|academic_year| where(academic_year: academic_year)}
 
