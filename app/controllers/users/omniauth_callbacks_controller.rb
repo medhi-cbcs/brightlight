64294b058f3c8e18e354b051b07c420c2cde466b
medhi <medhiwidjaja@yahoo.com>
2016-05-10 09:23:31 +0700

    Check in bin/ folder and changes in profile picture

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-    puts request.env["omniauth.auth"]
-    
+    # puts request.env["omniauth.auth"]
+
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       session[:user_id] = @user.id
