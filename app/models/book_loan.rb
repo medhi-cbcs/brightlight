60388ec3a79432002ad61dd8e080ca60522e808e
medhi <medhiwidjaja@yahoo.com>
2016-05-18 12:54:36 +0700

    Fixed grade display in BookCopy loans

--- app/models/book_loan.rb
+++ app/models/book_loan.rb
@@ -12,11 +12,6 @@ class BookLoan < ActiveRecord::Base
   scope :current, lambda { where(academic_year: AcademicYear.current) }
 
   def grade_section_name
-    subject_classes = ['CG011.1','CG011.2','CG012.1','CG012.2']
-    if subject_classes.include? grade_section_code
-      GradeSection.select(:name).where(subject_code: grade_subject_code).take.try(:name)
-    else
-      GradeSection.select(:name).where(parallel_code: grade_section_code).take.try(:name)
-    end
+    student.grade_section_with_academic_year_id(self.academic_year_id).try(:name)
   end
 end
