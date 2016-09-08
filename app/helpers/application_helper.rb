0eb50eb272281d5db9a4e60e79e520223b93f339
medhi <medhiwidjaja@yahoo.com>
2016-09-08 15:40:31 +0700

    Refactor sortable links It now has app-wide scope

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
     css_class = 'delete-link '+options[:class]
     link_to model, data: data_options, method: :delete, remote: true, class: css_class, &block
   end
+
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    icon = sort_direction == "asc" ? "keyboard_arrow_up" : "keyboard_arrow_down"
+    icon = column == sort_column ? icon : ""
+    link_to "#{title} <i class='material-icons vmiddle'>#{icon}</i>".html_safe, {column: column, direction: direction}
+  end
+
 end
