cfa35849f7e28bf189bc6c30b18970aa571d0315
medhi <medhiwidjaja@yahoo.com>
2016-09-13 15:37:41 +0700

    Removed debug puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,6 @@ class ApplicationController < ActionController::Base
   # rescue_from (ActiveRecord::RecordNotFound) { |exception| handle_exception(exception, 404) }
   
   def sort_column    
-    puts "Sort column: columns_to_sort: #{sortable_columns} include? #{params[:column]}"
     columns_to_sort = sortable_columns.map &:to_s
     columns_to_sort.include?(params[:column]) ? params[:column] : columns_to_sort.first
   end
