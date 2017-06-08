d6960242b1ab6ed94f48e10e57f8d020ab73fa60
medhi <medhiwidjaja@yahoo.com>
2017-06-08 11:23:09 +0700

    SQL to fix 9B books

---
+++ db/sql/check_9B_student_books.sql
@@ -0,0 +1,9 @@
+﻿select s.id, s.student_id, so.name, st.id, st.name, st.student_no
+from student_books s
+join grade_sections_students gss on cast(s.roster_no as integer) = gss.order_no 
+	and gss.academic_year_id = s.academic_year_id
+	and gss.grade_section_id = s.grade_section_id
+join students st on st.id = gss.student_id and gss.academic_year_id = 16
+join students so on so.id = s.student_id
+where s.academic_year_id = 16 and s.grade_section_id = 35
+order by cast(s.roster_no as integer), s.barcode
