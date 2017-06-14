e95ec75e10487cdbba65e608e587f5cb1c33d010
medhi <medhiwidjaja@yahoo.com>
2017-06-14 10:24:14 +0700

    Model Relations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,8 @@
 class Invoice < ActiveRecord::Base
   belongs_to :student
   belongs_to :user
+  belongs_to :academic_year
+  
   has_many   :line_items, dependent: :destroy
 
   validates :total_amount, presence: true
